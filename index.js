7671b221ee18192df5bf548e2fe11d8f0b9ea566
alexandre <alexandre.eberhardt@etu.utc.fr>
2025-07-05 19:16:18 +0200

    Initial commit: projet Node.js + Fastify + Prisma

---
+++ index.js
@@ -0,0 +1,24 @@
+const Fastify = require('fastify');
+const { PrismaClient } = require('@prisma/client');
+
+const fastify = Fastify({ logger: true });
+const prisma = new PrismaClient();
+
+fastify.get('/tests', async (request, reply) => {
+  const tests = await prisma.test.findMany();
+  return tests;
+});
+
+fastify.post('/tests', async (request, reply) => {
+  const { name } = request.body;
+  const test = await prisma.test.create({
+    data: { name },
+  });
+  reply.code(201);
+  return test;
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err;
+  fastify.log.info(`Server listening at ${address}`);
+});
